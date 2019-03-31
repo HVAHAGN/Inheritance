bef023dd6d29b9d523a1db53089a2761614bfcd6
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 23:15:10 +0400

    Virtual-Override methods

---
+++ Virtual_07/DerivedClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_07
+{
+    class DerivedClass:BaseClass
+    {
+        // Override base class method.
+        //The virtual keyword is used to modify a method, property, indexer, or event declared in the base class 
+        //and allow it to be overridden in the derived class. The override keyword is used to extend or modify
+        //a virtual/abstract method, property, indexer, or event of base class into derived class
+
+        public override void Method()
+        {
+            Console.WriteLine("Method from DerivedClass");
+        }
+
+    }
+}
---
+++ Virtual_07/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass instance = new DerivedClass();
+            instance.Method();
+
+            // UpCast
+            BaseClass instanceUp = instance;
+            instanceUp.Method();
+
+            // DownCast
+            DerivedClass instanceDown = (DerivedClass)instanceUp;
+            instanceDown.Method();
+
+            //Method from DerivedClass
+            //Method from DerivedClass
+            //Method from DerivedClass
+
+            Console.ReadLine();
+        }
+    }
+}
