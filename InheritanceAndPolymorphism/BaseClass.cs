a5b6e6f125db7e24326998c88e0d3d0f1707d8f2
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-03-31 20:28:33 +0400

    Subclasses inherit the features of one superclass.

---
+++ InheritanceAndPolymorphism/BaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphism
+{
+    class BaseClass
+    {
+        
+        public string publicField = "BasePublicField";
+        private string privateField = "BaseClassPrivateField";
+        protected string protectedField = "BaseClassProtected field";
+    }
+}
--- InheritanceAndPolymorphism/Program.cs
+++ InheritanceAndPolymorphism/Program.cs
@@ -7,9 +7,23 @@ using System.Threading.Tasks;
 namespace InheritanceAndPolymorphism
 {
     class Program
-    {
+    { //Subclasses inherit the features of one superclass.Inheritance
+      // Inheritance.
+
+        // Inheritance is an object-oriented programming mechanism that allows you to describe a new (derived) class
+        // based on the already existing (base)
+        // with this, the properties and functionality of the base class are borrowed by the new derived class.
+
+        // Base class - Derived class
+        // Super class - Subclass or (subclass)
+        // Parent Class - Child Class
+        // Parent - Child
+
         static void Main(string[] args)
         {
+
+            DerivedClass instance = new DerivedClass();
+            Console.WriteLine(instance.publicField);
             Console.ReadLine();
         }
     }
